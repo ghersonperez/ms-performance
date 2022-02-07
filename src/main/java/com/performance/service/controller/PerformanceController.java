da9645c8c20b9aebaa0b5fb5dfb4dc88bb1ab7f5
Gherson Alberto Pérez Choque <ghersonalberto.perezchoque@telefonicatgt.es>
2022-02-07 13:09:46 -0500

    dsd

--- src/main/java/com/performance/service/controller/PerformanceController.java
+++ src/main/java/com/performance/service/controller/PerformanceController.java
@@ -23,6 +23,7 @@ import com.performance.shared.dto.PageResponseDTO;
 
 @RestController()
 @RequestMapping("/performance/v1/api")
+@CrossOrigin("*")
 public class PerformanceController {
 
 	@Autowired
